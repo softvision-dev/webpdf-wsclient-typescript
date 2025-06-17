32ca754f5327d21372aef311159b13e171e5ab42
Sebastian Marlog <sea@softvision.de>
2025-06-17 11:54:25 +0200

    OpenAPI update
    extend OpenAPI schema with new configuration-related definitions and properties. Improved description formatting and adjusted `x-webpdf-codegen` structure for consistency. Replace `node-fetch` with `axios` in deploy.js for HTTP requests. Simplify error handling and maintain existing functionality.

--- deploy.js
+++ deploy.js
@@ -2,69 +2,63 @@
 
 const {execSync} = require('child_process');
 const fs = require("fs");
-const fetch = require("node-fetch");
-const {Headers} = require("node-fetch");
 const {URL} = require('url');
 const FormData = require('form-data');
 const Path = require('path');
+const axios = require("axios");
 
 const configPath = Path.join(__dirname, "config", "buildConfig.json");
 
 (async () => {
-	try {
-		console.log('-- build project --');
-		execSync('yarn run build', {stdio: "inherit"});
+    try {
+        console.log('-- build project --');
+        execSync('yarn run build', {stdio: "inherit"});
 
-		let config = JSON.parse(fs.readFileSync(configPath).toString());
+        let config = JSON.parse(fs.readFileSync(configPath).toString());
 
-		if (config.publish === "local") {
-			console.log('-- pack project --');
-			execSync('npm pack', {stdio: "inherit"});
+        if (config.publish === "local") {
+            console.log('-- pack project --');
+            execSync('npm pack', {stdio: "inherit"});
 
-			let version = process.env.npm_package_version;
-			let packageName = 'softvision-webpdf-wsclient-typescript-' + version + '.tgz';
-			let readStream = fs.createReadStream(packageName);
+            let version = process.env.npm_package_version;
+            let packageName = 'softvision-webpdf-wsclient-typescript-' + version + '.tgz';
+            let readStream = fs.createReadStream(packageName);
 
-			const formData = new FormData();
-			formData.append('file', readStream);
+            const formData = new FormData();
+            formData.append('file', readStream);
 
-			let headers = new Headers();
-			headers.append('Accept', 'application/json');
-			headers.append(
-				'Authorization',
-				'Basic ' + Buffer.from(config.username + ":" + config.password).toString("base64")
-			);
+            let url = new URL(config.api);
+            url.searchParams.set("repository", config.repository);
 
-			let url = new URL(config.api);
-			url.searchParams.set("repository", config.repository);
-			let response = await fetch(url, {
-				method: "POST",
-				headers: headers,
-				body: formData
-			});
+            let response = await axios.post(url, formData, {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: 'Basic ' + Buffer.from(config.username + ":" + config.password).toString("base64")
+                }
+            });
 
-			if (!response.ok) {
-				let error = new Error(response.statusText);
-				error.code = response.status;
-				error.stack = await response.text();
-				throw error;
-			}
-		} else {
-			// public publish needs .npmrc file with: //registry.npmjs.org/:_authToken=<TOKEN>
-			let version = process.env.npm_package_version;
+            if (response.status >= 200 && response.status < 300) {
+                let error = new Error(response.statusText);
+                error.code = response.status;
+                error.stack = await response.data;
+                throw error;
+            }
+        } else {
+            // public publish needs .npmrc file with: //registry.npmjs.org/:_authToken=<TOKEN>
+            let version = process.env.npm_package_version;
 
-			console.log('-- deploy project --');
-			execSync('yarn publish --access=public --always-auth=true --non-interactive --new-version ' + version, {stdio: "inherit"});
-		}
-	} catch (err) {
-		if (typeof err.stderr !== 'undefined' && err.stderr !== null && err.stderr.toString() !== '') {
-			console.error(err.stderr.toString());
-		} else if (typeof err.stdout !== 'undefined' && err.stdout !== null && err.stdout.toString() !== '') {
-			console.error(err.stdout.toString());
-		} else {
-			console.error(err);
-		}
+            console.log('-- deploy project --');
+            execSync('yarn publish --access=public --always-auth=true --non-interactive --new-version ' + version, {stdio: "inherit"});
+        }
+    } catch (err) {
+        if (typeof err.stderr !== 'undefined' && err.stderr !== null && err.stderr.toString() !== '') {
+            console.error(err.stderr.toString());
+        } else if (typeof err.stdout !== 'undefined' && err.stdout !== null && err.stdout.toString() !== '') {
+            console.error(err.stdout.toString());
+        } else {
+            console.error(err);
+        }
 
-		process.exit(1);
-	}
+        process.exit(1);
+    }
 })();
